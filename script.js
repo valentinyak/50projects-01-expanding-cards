f874d6e4e848641556f6691d585afe07632b8605
valentinyak <63512456+valentinyak@users.noreply.github.com>
2022-04-15 00:25:13 +0300

    optimize js code

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 const allPanels = document.querySelectorAll('.container>li');
 
-for (let i = 0; i < allPanels.length; i += 1) {
-  const panel = allPanels[i];
-
+allPanels.forEach(panel => {
   panel.addEventListener('click', () => {
-    const activePanel = document.querySelector('.active');
+    allPanels.forEach(panel => {
+      panel.classList.remove('active');
+    });
 
-    if (panel != activePanel) {
-      activePanel.classList.remove('active');
-      panel.classList.add('active');
-    }
+    panel.classList.add('active');
   });
-}
+});
